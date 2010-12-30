e5f3b24d4cea7d78182e48350af88351cf1fc4ca
Karthik <karthikjagadeesh@gmail.com>
2010-12-30 12:41:34 -0800

    a form to update the twitter status

---
+++ src/update_status.php
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body>
+
+<form method="post" action="get_status.php">
+Enter the Status Here: <input type="text" name="status" size="100"/>
+<input type="submit" value="Submit" />
+</form>
+</body>
+</html>
